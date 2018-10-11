57b8ad34e66a68c6e3e8a14cc913b4dad9b82134
danik529 <lsy12011@naver.com>
2018-10-11 15:36:03 +0900

    control_2

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[]) {
 	
 	int i;
+	int a; //절대값을 저장하는 변 
 
 	
 	printf("input an integer : ");
@@ -13,15 +14,12 @@ int main(int argc, char *argv[]) {
 	
 	if (i > 0)
 	{
-	    printf("양 수 입 니 다.");
-    }
-    else if (i == 0)
-	{
-	    printf("0 입니다.");
+	   a = i;
     }
 	else
 	{
-	    printf("음수입니다.");
+	   a = -i;
     }
+    printf("절대값은 %i입니다.", a);
 	return 0;
 }
